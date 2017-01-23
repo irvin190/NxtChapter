bc052e5ba5717641bb23ac079dd50dc342a58994
Irvin <irvinrodr24@gmail.com>
2017-01-23 18:37:39 -0500

    added heroku sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,14 @@ class StaticPagesController < ApplicationController
     @test_variable = 42
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'irvin.umbc@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
 end
