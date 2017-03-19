387327ccdebbbc8a2ebae7cdded70f20d35d77a5
Irvin Rodriguez <irvinrodr24@gmail.com>
2017-03-18 22:30:54 -0400

    5.9 Validation & Pagination_navbar edit

--- Gemfile
+++ Gemfile
@@ -48,8 +48,10 @@ group :production do
 	gem 'pg'
 end
 
-gem 'devise'
-gem 'cancancan', '~>1.10'
+  gem 'devise'
+  gem 'cancancan', '~>1.10'
+  # Gemfile for Rails 3+, Sinatra, and Merb
+  gem 'will_paginate', '~> 3.1.1'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true} #If you want the users to message each other, then do not validate this line
+
   scope :rating_desc, -> {order(rating: :desc)}
   scope :moola, -> {order(rating: :asc)}
 end
